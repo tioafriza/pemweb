5101c32837cf87c77702ae0067ac4b75be5d1621
tioafriza <yoktiyok15@gmail.com>
2025-07-20 00:38:09 +0700

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ function About() {
   return (
     <div className="container">
       <h1>Tentang Aplikasi</h1>
-      <p>Aplikasi ini dibuat oleh mahasiswa untuk UTS Pemrograman Web.</p>
+      <p>Aplikasi ini dibuat oleh mahasiswa untuk Project UTS (Frontend) dan UAS (Backend) Pemrograman Web.</p>
     </div>
   );
 }
